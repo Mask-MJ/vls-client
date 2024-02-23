bb398b6e8361e028ca45a952169939dd6038a310
Lizzie <820597849@qq.com>
2024-02-23 20:42:19 +0800

    fix: 修改注释

--- src/api/valve.ts
+++ src/api/valve.ts
@@ -6,15 +6,15 @@ enum Api {
   Valves = 'valves'
 }
 
-// 获取工厂列表
+// 获取阀门列表
 export const getValveList = (params?: Partial<SearchParams>) =>
   defHttp.get<ValveInfo[]>({ url: Api.Valves, params })
-// 创建工厂
+// 创建阀门
 export const createValve = (params: CreatedValve) => defHttp.post({ url: Api.Valves, params })
-// 获取单个工厂信息
+// 获取单个阀门信息
 export const getValveDetail = (id: number) => defHttp.get<ValveInfo>({ url: `${Api.Valves}/${id}` })
-// 更新工厂
+// 更新阀门
 export const updateValve = (params: Partial<ValveInfo>) =>
   defHttp.patch({ url: `${Api.Valves}/${params.id}`, params })
-// 删除工厂
+// 删除阀门
 export const deleteValve = (ids: number | string) => defHttp.delete({ url: `${Api.Valves}/${ids}` })
